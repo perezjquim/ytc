9178c92115d0ff51d6410e712520411b51bd4b94
Joaquim Perez <jquim1@hotmail.com>
2021-07-24 14:49:01 +0100

    headers improv

--- handlers/APIHandler.py
+++ handlers/APIHandler.py
@@ -44,6 +44,7 @@ class APIHandler( ):
 		print( '> Preparing request' )	
 		succ_response = send_from_directory( video_tmp_directory, filename = video_ffmpeg_filename, as_attachment = True  )
 		succ_response.headers[ 'Content-Disposition' ] = "attachment; filename={};".format( video_output_filename )
+		succ_response.headers[ 'Access-Control-Expose-Headers' ] = 'Content-Disposition'
 		print( '< Preparing request' )			
 
 		return succ_response
