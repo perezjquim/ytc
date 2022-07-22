2238b03394ee182d3f89b96f80ace3ef700c3cd3
Joaquim Perez <jquim1@hotmail.com>
2022-07-22 20:14:18 +0100

    timeout param increase / error handling improv

--- handlers/APIHandler.py
+++ handlers/APIHandler.py
@@ -21,69 +21,81 @@ class APIHandler( ):
 
 	@api.route( '/crop-video', methods = [ 'GET' ] )
 	def crop_video( ):
-		args = request.args
+		try:
 
-		current_datetime = datetime.now( )
-		current_datetime_str = current_datetime.isoformat( )
+			args = request.args
 
-		video_tmp_directory = './'	
+			current_datetime = datetime.now( )
+			current_datetime_str = current_datetime.isoformat( )
 
-		url_encoded = quote( args[ 'url' ], safe = '' 	)
-		video_ydl_filename = 'tmp-ydl - {}.mp4'.format( url_encoded )	
+			video_tmp_directory = './'	
 
-		print( '> Downloading' )
-		if os.path.exists( '{}/{}'.format( video_tmp_directory, video_ydl_filename ) ):
-			print( '< Downloading.. done (already downloaded)!' )
-		else:
-			ydl_opts = { "format": "18", "outtmpl": video_ydl_filename }
-			with YoutubeDL( ydl_opts ) as ydl:
-    				ydl.download( [ args[ 'url' ] ] )			
-			print( '< Downloading.. done!' )			
+			url_encoded = quote( args[ 'url' ], safe = '' 	)
+			video_ydl_filename = 'tmp-ydl - {}.mp4'.format( url_encoded )	
 
-		video_output_filename = 'YTC - {}.mp4'.format( current_datetime_str )	
-		video_ffmpeg_filename = 'tmp-ff - {}'.format( video_output_filename )		
+			print( '> Downloading' )
+			if os.path.exists( '{}/{}'.format( video_tmp_directory, video_ydl_filename ) ):
+				print( '< Downloading.. done (already downloaded)!' )
+			else:
+				ydl_opts = { "format": "18", "outtmpl": video_ydl_filename }
+				with YoutubeDL( ydl_opts ) as ydl:
+						ydl.download( [ args[ 'url' ] ] )			
+				print( '< Downloading.. done!' )			
 
-		start_datetime = datetime.strptime( args[ 'start_time' ], "%M:%S" )
-		start_timedelta = start_datetime - datetime( 1900, 1, 1 )
-		start_time_in_seconds = start_timedelta.total_seconds( )
+			video_output_filename = 'YTC - {}.mp4'.format( current_datetime_str )	
+			video_ffmpeg_filename = 'tmp-ff - {}'.format( video_output_filename )		
 
-		end_datetime = datetime.strptime( args[ 'end_time' ], "%M:%S" )
-		end_timedelta = end_datetime - datetime( 1900, 1, 1 )
-		end_time_in_seconds = end_timedelta.total_seconds( )		
+			start_datetime = datetime.strptime( args[ 'start_time' ], "%M:%S" )
+			start_timedelta = start_datetime - datetime( 1900, 1, 1 )
+			start_time_in_seconds = start_timedelta.total_seconds( )
 
-		print( '> Cutting' )
-		clip = VideoFileClip( video_ydl_filename ).subclip( start_time_in_seconds, end_time_in_seconds )
-		clip.write_videofile( filename = video_ffmpeg_filename, codec = "libx264", temp_audiofile = 'temp-audio.m4a', remove_temp = True, audio_codec = 'aac' )		
-		clip.close( )
-		print( '< Cutting' )		
+			end_datetime = datetime.strptime( args[ 'end_time' ], "%M:%S" )
+			end_timedelta = end_datetime - datetime( 1900, 1, 1 )
+			end_time_in_seconds = end_timedelta.total_seconds( )		
 
-		print( '> Preparing request' )	
-		succ_response = send_from_directory( video_tmp_directory, filename = video_ffmpeg_filename, as_attachment = True  )
-		succ_response.headers[ 'Content-Disposition' ] = "attachment; filename={};".format( video_output_filename )
-		succ_response.headers[ 'Access-Control-Expose-Headers' ] = 'Content-Disposition'
-		print( '< Preparing request' )		
+			print( '> Cutting' )
+			clip = VideoFileClip( video_ydl_filename ).subclip( start_time_in_seconds, end_time_in_seconds )
+			clip.write_videofile( filename = video_ffmpeg_filename, codec = "libx264", temp_audiofile = 'temp-audio.m4a', remove_temp = True, audio_codec = 'aac' )		
+			clip.close( )
+			print( '< Cutting' )		
 
-		print( '!! DONE !!' )	
+			print( '> Preparing request' )	
+			succ_response = send_from_directory( video_tmp_directory, filename = video_ffmpeg_filename, as_attachment = True  )
+			succ_response.headers[ 'Content-Disposition' ] = "attachment; filename={};".format( video_output_filename )
+			succ_response.headers[ 'Access-Control-Expose-Headers' ] = 'Content-Disposition'
+			print( '< Preparing request' )		
 
-		return succ_response
+			print( '!! DONE !!' )	
+
+			return succ_response
+
+		except Exception as e:
+
+			return Response( str( e ), status = 500 )
 
 	@api.route( '/get-video-info', methods = [ 'GET' ] )
 	def get_video_info( ):
-		args = request.args
+		try:
+			
+			args = request.args
 
-		with YoutubeDL({}) as ydl:
-		      	video_info = ydl.extract_info( args[ 'url' ], download = False )
-		      	title = video_info.get( 'title' )
-		      	thumbnail_url = video_info.get( 'thumbnails' )[ 0 ][ 'url' ]
-		      	author = video_info.get( 'uploader' )
-		      	duration = str( timedelta( seconds = video_info.get( 'duration' ) ) )
+			with YoutubeDL({}) as ydl:
+					video_info = ydl.extract_info( args[ 'url' ], download = False )
+					title = video_info.get( 'title' )
+					thumbnail_url = video_info.get( 'thumbnails' )[ 0 ][ 'url' ]
+					author = video_info.get( 'uploader' )
+					duration = str( timedelta( seconds = video_info.get( 'duration' ) ) )
 
-		      	return jsonify({
-				'title': title,
-				'thumbnail_url': thumbnail_url,
-				'author': author,
-				'duration': duration
-			})
+					return jsonify({
+					'title': title,
+					'thumbnail_url': thumbnail_url,
+					'author': author,
+					'duration': duration
+				})
+
+		except Exception as e:
+
+			return Response( str( e ), status = 500 )				
 
 	def _get_video( url ):
 		proxies = APIHandler._get_proxies( )
@@ -126,10 +138,3 @@ class APIHandler( ):
 					print( '< Fetching proxies.. done!' )					
 
 					return proxies
-
-
-
-
-		
-
-		
